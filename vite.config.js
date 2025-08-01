4174144bbf0dc4bdc9246abbc61d5923e185c234
Arnold208 <arnold.kimkpe@afrilogicsolutions.com>
2025-08-01 19:06:22 +0000

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/kafuis_frontend",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
